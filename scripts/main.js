1f523d24e045733ba5a19986652b9b665cc22670
NoxLP <nox_nox_@hotmail.com>
2020-11-09 22:11:17 +0000

    Player movement and boundaries.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,46 @@
-//Import view.js file for storing the canvas in it
-import * as View from "./view/view.js"
+const STEP = 30;
+function Canvas(){
+  this.elem = document.getElementById('game');
+  this.width = 1100;
+  this.height = 800;
 
-window.onload = () => {
-    //send canvas to view and store it
-    View.setCanvas(document.querySelector("canvas"));
+  this.elem.style.width = `${this.width}px`;
+  this.elem.style.height = `${this.height}px`;
 };
+const canvas = new Canvas();
+
+function Player() {
+  this.elem = document.getElementById('player');
+  this.width = 80;
+  this.height = 80;
+  this.x = Math.round((canvas.width / 2) - (this.width / 2));
+  this.y = 60;
+  
+  this.update = function() {
+    this.elem.style.width = `${this.width}px`;
+    this.elem.style.height = `${this.height}px`;
+    this.elem.style.left = `${this.x}px`;
+    this.elem.style.bottom = `${this.y}px`;
+  }
+  
+  this.update();
+  this.moveLeft = function() {
+    if (this.x > STEP) {    
+      this.x -= STEP;
+      this.update();
+    }
+  }
+
+  this.moveRight = function() {
+    if (this.x + this.width < canvas.width - STEP) {
+      this.x += STEP;
+      this.update();
+    }
+  }
+}
+const player = new Player();
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") { player.moveLeft() }
+  if (e.key === "ArrowRight") { player.moveRight() }
+})
