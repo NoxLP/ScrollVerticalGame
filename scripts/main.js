55ce28b9b50eaa98a67649773e6258877a66b8cd
NoxLP <nox_nox_@hotmail.com>
2020-11-10 12:46:14 +0000

    Changed main.js to pick player and enemies size and initial coordinates from game object.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,12 @@
 import { DrawableObject } from "./drawableObject.js";
 
-const ENEMIES_SIZES = [
-  [50,50],
-  [65,65],
-  [80,80]
-];
-const PLAYER_SIZE = [80, 80];
 
 class Canvas {
   constructor() {
     this.elem = document.getElementById('game');
     this.width = 1100;
     this.height = 800;
-
+    
     this.elem.style.width = `${this.width}px`;
     this.elem.style.height = `${this.height}px`;
   }
@@ -22,29 +16,38 @@ class Game {
   constructor(enemiesPerRow) {
     this.step = 30;
     this.elem = document.getElementById("game");
-    this.enemies = new Array(5).fill(new Array(11));
+    this.enemiesSize = [
+      [50,50],
+      [65,65],
+      [80,80]
+    ];
+    this.playerSize = [80, 80];
+    //console.log(canvas.width)
+    this.playerInitialCoords = [Math.round((canvas.width / 2) - (this.playerSize[0] / 2)), 60]
     this.enemiesPerRow = enemiesPerRow;
-    this._fillEnemies();
+    this.enemies = new Array(5).fill(new Array(this.enemiesPerRow));
   }
   _calculateCoordinatesByPosition(column, row) {
-    console.log(this.elem.style.width)
-    console.log(this.enemiesPerRow)
+    //console.log(this.elem.style.width)
+    //console.log(this.enemiesPerRow)
     return [
       (parseInt(this.elem.style.width) / this.enemiesPerRow) * column,
-      (parseInt(this.elem.style.height) / this.enemiesPerRow) * row
+      (parseInt(this.elem.style.height - this.playerSize[1] - this.enemiesSize[2] - this.playerInitialCoords[1]) / this.enemiesPerRow) * row
     ];
   }
-  _fillEnemies() {
+  createEnemies() {
     //loop per enemy type
     for (let type = 0; type < 3; type++) {
       if (type == 0) {
         for(let i = 0; i < this.enemiesPerRow; i++) {
           const coords = this._calculateCoordinatesByPosition(i, 0);
-          console.log(coords)
+          //console.log(coords)
           this.enemies[type][i] = new Enemy(type, coords[0], coords[1]);
           this.elem.appendChild(this.enemies[type][i].elem);
         }
       } else {
+
+        //const coords = this._calculateCoordinatesByPosition(i, 0);
 
       }
     }
@@ -56,19 +59,19 @@ class Enemy extends DrawableObject {
     let elem = new Image();
     elem.src = `../assets/images/spaceships/enemy${type}.png`;
     elem.classList.add("enemy");
-    super(elem, x, y, ENEMIES_SIZES[type][0], ENEMIES_SIZES[type][1]);
+    super(elem, x, y, game.enemiesSize[type][0], game.enemiesSize[type][1]);
 
     this.type = type;
   }
 }
 
 class Player extends DrawableObject {
-  constructor(width, height) {
+  constructor() {
     let elem = new Image(); //document.getElementById('player');
     elem.src = "../assets/images/spaceships/player1.png";
     elem.id = "player";
-    super(elem, Math.round((canvas.width / 2) - (width / 2)), 60, width, height, "bottom")
-
+    super(elem, game.playerInitialCoords[0], game.playerInitialCoords[1], game.playerSize[0], game.playerSize[1], "bottom")
+    
     game.elem.appendChild(this.elem);
   }
   moveLeft = function () {
@@ -78,8 +81,8 @@ class Player extends DrawableObject {
     }
   }
   moveRight = function () {
-    if (this.x + this.width < canvas.width - STEP) {
-      this.x += STEP;
+    if (this.x + this.width < canvas.width - game.step) {
+      this.x += game.step;
       this.update();
     }
   }
@@ -87,7 +90,8 @@ class Player extends DrawableObject {
 
 const canvas = new Canvas();
 const game = new Game(11);
-const player = new Player(PLAYER_SIZE[0], PLAYER_SIZE[1]);
+game.createEnemies();
+const player = new Player();
 console.log(game);
 
 document.addEventListener("keydown", function (e) {
